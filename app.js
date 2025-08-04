a044f038d9a7d95150f7be3cc031950c2b736c9e
Msalermo4 <alvardito92@gmail.com>
2025-08-03 20:11:49 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+async function searchCases(query) {
+  const response = await fetch(`https://www.courtlistener.com/api/rest/v3/search/?type=o&q=${encodeURIComponent(query)}`, {
+    headers: { "Authorization": `Token ${API_KEY}` }
+  });
+  const data = await response.json();
+  const resultsDiv = document.getElementById("results");
+  resultsDiv.innerHTML = "";
+  data.results.forEach(item => {
+    const div = document.createElement("div");
+    div.innerHTML = `<strong>${item.caseName}</strong><br><a href="${item.absolute_url}" target="_blank">Read Opinion</a><hr>`;
+    resultsDiv.appendChild(div);
+  });
+}
 
